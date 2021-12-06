78d96db35339c2336892ccfad14806b860e55567
Konstantinos Persimintzis <kpersimintzis@gmail.com>
2021-12-06 16:07:21 +0200

    Uri absolute vs relative and convertions

--- Spider/Program.cs
+++ Spider/Program.cs
@@ -7,7 +7,6 @@ using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System.IO;
-using Microsoft.Extensions.Configuration;
 //using Microsoft.Extensions.Configuration.FileExtensions;
 using Microsoft.Extensions.Configuration.Json;
 using System.Net;
@@ -42,17 +41,32 @@ namespace Spider
         {
             using (WebClient client = new WebClient())
             {
-                string htmlCode = client.DownloadString(root);
+                Uri rootUri = new Uri(root);
+                string htmlCode = client.DownloadString(rootUri);
                 var htmlDoc = new HtmlDocument();
                 htmlDoc.LoadHtml(htmlCode);
                 var nodes = htmlDoc.DocumentNode.Descendants("a").Where(x => x.Attributes["href"] != null);
                 foreach (var node in nodes)
                 {
-                    Console.WriteLine($"name:{node.Attributes["href"].Name}, value: {node.Attributes["href"].Value}");
+                    if (Uri.TryCreate(node.Attributes["href"].Value, UriKind.RelativeOrAbsolute, out var _uri))
+                    {
+                        if (!_uri.IsAbsoluteUri && Uri.TryCreate(rootUri, _uri, out var _combinedUri))
+                        {
+                            Console.WriteLine($"name:{node.Attributes["href"].Name}, value: {node.Attributes["href"].Value}" +
+                                            $"is absolute? : {_uri.IsAbsoluteUri} after combined : {_combinedUri.IsAbsoluteUri} " +
+                                            $"new Uri: {_combinedUri.AbsoluteUri}");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine($"name:{node.Attributes["href"].Name}, value: {node.Attributes["href"].Value}" +
+                                            $"is absolute? : {_uri.IsAbsoluteUri}");
+                        }
+                    }
                 }
             }
             return;
-            
+
             using (var context = new SpiderDBContextFactory().CreateDbContext(null))
             {
                 var pages = context.Pages.AsQueryable();
