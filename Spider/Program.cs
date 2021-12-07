5a5b61320ad533e9475558984d2f9bbe26f6eabe
AquamarineBlue0 <isminivasileiou0@gmail.com>
2021-12-07 13:53:36 +0200

    7.12

--- Spider/Program.cs
+++ Spider/Program.cs
@@ -11,12 +11,12 @@ using System.IO;
 using Microsoft.Extensions.Configuration.Json;
 using System.Net;
 using HtmlAgilityPack;
+using System.Collections.Generic;
 
 namespace Spider
 {
     class Program
     {
-
         public class SpiderDBContextFactory : IDesignTimeDbContextFactory<SpiderDbContext>
         {
             public readonly ILoggerFactory _loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
@@ -33,38 +33,53 @@ namespace Spider
 
                 return new SpiderDbContext(optionsBuilder.Options);
             }
-
         }
 
-        public const string root = "https://en.wikipedia.org/wiki/Web_crawler";
-        static void Main(string[] args)
+        public static void Walk(Uri uri, HashSet<string> visited)
         {
+            if (visited.Contains(uri.AbsoluteUri)) return;
+            
             using (WebClient client = new WebClient())
             {
-                Uri rootUri = new Uri(root);
-                string htmlCode = client.DownloadString(rootUri);
+                visited.Add(uri.AbsoluteUri);
+                string htmlCode = "";
+                try
+                {
+                    htmlCode = client.DownloadString(uri);
+                }
+                catch (WebException) { return; }
+                
                 var htmlDoc = new HtmlDocument();
                 htmlDoc.LoadHtml(htmlCode);
                 var nodes = htmlDoc.DocumentNode.Descendants("a").Where(x => x.Attributes["href"] != null);
+
                 foreach (var node in nodes)
                 {
                     if (Uri.TryCreate(node.Attributes["href"].Value, UriKind.RelativeOrAbsolute, out var _uri))
                     {
-                        if (!_uri.IsAbsoluteUri && Uri.TryCreate(rootUri, _uri, out var _combinedUri))
+                        if (!_uri.IsAbsoluteUri && Uri.TryCreate(uri, _uri, out var _combinedUri))
                         {
                             Console.WriteLine($"name:{node.Attributes["href"].Name}, value: {node.Attributes["href"].Value}" +
                                             $"is absolute? : {_uri.IsAbsoluteUri} after combined : {_combinedUri.IsAbsoluteUri} " +
                                             $"new Uri: {_combinedUri.AbsoluteUri}");
-
+                            Walk(_combinedUri, visited);
                         }
                         else
                         {
                             Console.WriteLine($"name:{node.Attributes["href"].Name}, value: {node.Attributes["href"].Value}" +
                                             $"is absolute? : {_uri.IsAbsoluteUri}");
+                            Walk(_uri, visited);
                         }
                     }
                 }
             }
+        }
+
+        public const string root = "https://en.wikipedia.org/wiki/Web_crawler";
+        static void Main(string[] args)
+        {
+            Walk(new Uri(root), new HashSet<string>());
+            
             return;
 
             using (var context = new SpiderDBContextFactory().CreateDbContext(null))
